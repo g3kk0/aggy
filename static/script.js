23e00b1e38f33551d3aa2fcaae6a293bd7c829c5
Mark Newman <mark.newman@r3labs.io>
2018-11-22 23:49:57 +0000

    Fix UI

--- static/script.js
+++ static/script.js
@@ -6,11 +6,16 @@ function fetchValues() {
 	    return resp.json();
         })
 	.then(data => {
-            document.getElementById("value-subtitle").style.visibility = "visible";
-            document.getElementById("pnl-subtitle").style.visibility = "visible";
-            document.getElementById("pnlPc").innerHTML = data.pnl_pc.toFixed(2) + " %";
-            document.getElementById("value").innerHTML = "£" + data.value.toFixed(2);
-            document.getElementById("pnl").innerHTML = "£" + data.pnl.toFixed(2);
+	    document.getElementById("loading").style.display = "none";
+            document.getElementById("title").style.display = "flex";
+            document.getElementById("subtitle-1").style.display = "flex";
+            document.getElementById("subtitle-2").style.display = "flex";
+		
+            // document.getElementById("value-subtitle").style.visibility = "visible";
+            // document.getElementById("pnl-subtitle").style.visibility = "visible";
+            // document.getElementById("pnlPc").innerHTML = data.pnl_pc.toFixed(2) + " %";
+            // document.getElementById("value").innerHTML = "£" + data.value.toFixed(2);
+            // document.getElementById("pnl").innerHTML = "£" + data.pnl.toFixed(2);
             console.log(data);
         })
 	.catch(err => {
