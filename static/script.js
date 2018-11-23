e4d7c78c1a48f80c4a57473cb65105d37eaca97a
Mark Newman <mark.newman@aldgate-ventures.com>
2018-11-23 18:18:37 +0000

    Fix UI

--- static/script.js
+++ static/script.js
@@ -10,12 +10,9 @@ function fetchValues() {
             document.getElementById("title").style.display = "flex";
             document.getElementById("subtitle-1").style.display = "flex";
             document.getElementById("subtitle-2").style.display = "flex";
-		
-            // document.getElementById("value-subtitle").style.visibility = "visible";
-            // document.getElementById("pnl-subtitle").style.visibility = "visible";
-            // document.getElementById("pnlPc").innerHTML = data.pnl_pc.toFixed(2) + " %";
-            // document.getElementById("value").innerHTML = "£" + data.value.toFixed(2);
-            // document.getElementById("pnl").innerHTML = "£" + data.pnl.toFixed(2);
+            document.getElementById("pnlPc").innerHTML = data.pnl_pc.toFixed(2) + " %";
+            document.getElementById("value").innerHTML = "£" + data.value.toFixed(2);
+            document.getElementById("pnl").innerHTML = "£" + data.pnl.toFixed(2);
             console.log(data);
         })
 	.catch(err => {
