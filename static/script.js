5d1a66c7937d71e8a8a04b34000e8af93e4413d9
Mark Newman <mark.newman@aldgate-ventures.com>
2019-05-14 11:14:50 +0100

    Style adjustments

--- static/script.js
+++ static/script.js
@@ -10,12 +10,19 @@ function fetchValues() {
             document.getElementById("title").style.display = "flex";
             document.getElementById("subtitle-1").style.display = "flex";
             document.getElementById("subtitle-2").style.display = "flex";
+            if (data.pnl > 0) {
+                document.getElementById("pnlPc").style.color = "#00C853";
+                document.getElementById("pnl").style.color = "#00C853";
+            } else {
+                document.getElementById("pnlPc").style.color = "#D50000";
+                document.getElementById("pnl").style.color = "#D50000";
+            }
             document.getElementById("pnlPc").innerHTML = data.pnl_pc.toFixed(2) + " %";
             document.getElementById("value").innerHTML = "£" + data.value.toFixed(2);
             document.getElementById("pnl").innerHTML = "£" + data.pnl.toFixed(2);
             console.log(data);
         })
 	.catch(err => {
-            // Do something for an error here
+	    console.log(err);
         });
 }
